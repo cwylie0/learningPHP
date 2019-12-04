52fdc14bf83c7bf6a3f73d07365fe272830de1f1
cwylie0 <cwylie0@hotmail.com>
2019-12-04 15:54:23 -0500

    update files

--- index.php
+++ index.php
@@ -1,24 +1,42 @@
-<html>
-<body>
-<form method="get" action="greet_user.php">
-Name:
-<input type="text" name="name">
-<br>
-Street:
-<input type="text" name="street">
-<br>
-City:
-<input type="text" name="city">
-<br>
-State:
-<input type="text" name="state">
-<br>
-Phone:
-<input type="text" name="phone">
-<br>
-<input type="submit" value="Submit">
+<?php
+// Define checkWord() here:
+function checkWord($input, $letter){
+   if ($_SERVER["REQUEST_METHOD"] === "POST" && strtolower($input[0]) != $letter) {
+    return "* This word must start with the letter ${letter}!";
+   } else {
+     return "";
+   }
+}  
+  
+?>
+  
+<h1>Time to Practice our ABCs</h1>
+<form method="post" action="">
+    Enter a word that starts with the letter "a":
+    <br>
+    <input type="text" name="a-word" id="a-word">
+      <p id="a-error"><?= checkWord($_POST["a-word"], "a");?></p>
+    <br>      
+      
+    <br>     
+    Enter a word that starts with the letter "b":
+    <br>
+    <input type="text" id="b-word" name="b-word">
+      <p id="b-error"><?= checkWord($_POST["b-word"], "b");?></p>
+    <br>      
+      
+    <br>
+    Enter a word that starts with the letter "c":
+    <br>
+    <input type="text" id="c-word" name="c-word">
+      <p id="c-error"><?= checkWord($_POST["c-word"], "c");?></p>
+    <br>      
+      
+    <br>
+    <input type="submit" value="Submit Words">
 </form>
-
-<a href="index.php">Reset</a>
-</body>
-</html>
+<div>
+    <h3>"a" is for: <?= $_POST["a-word"];?><h3>
+    <h3>"b" is for: <?= $_POST["b-word"];?><h3>
+    <h3>"c" is for: <?= $_POST["c-word"];?><h3>    
+<div>  
---
+++ indexFORM.php
@@ -0,0 +1,24 @@
+<html>
+<body>
+<form method="get" action="greet_user.php">
+Name:
+<input type="text" name="name">
+<br>
+Street:
+<input type="text" name="street">
+<br>
+City:
+<input type="text" name="city">
+<br>
+State:
+<input type="text" name="state">
+<br>
+Phone:
+<input type="text" name="phone">
+<br>
+<input type="submit" value="Submit">
+</form>
+
+<a href="index.php">Reset</a>
+</body>
+</html>
